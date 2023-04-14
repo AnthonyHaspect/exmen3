96eacf32c2184622609a333b4913c95ca66d5d55
Anthony Haspect <2027425@cegepoutaouais.qc.ca>
2023-04-14 10:07:44 -0400

    10h commit

---
+++ app/src/main/java/com/anthonyhaspect/anthonyhaspect/AjoutVoiture.java
@@ -0,0 +1,14 @@
+package com.anthonyhaspect.anthonyhaspect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjoutVoiture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajout_voiture);
+    }
+}
--- app/src/main/java/com/anthonyhaspect/anthonyhaspect/VoitureAdapteur.java
+++ app/src/main/java/com/anthonyhaspect/anthonyhaspect/VoitureAdapteur.java
@@ -1,4 +1,37 @@
 package com.anthonyhaspect.anthonyhaspect;
 
-public class VoitureAdapteur {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class VoitureAdapteur extends ArrayAdapter<Voiture> {
+    private Context context;
+    private LayoutInflater inflater;
+
+    public VoitureAdapteur(Context context, ArrayAdapter<Voiture> tabVoiture){
+        super(context, 0, (List<Voiture>) tabVoiture);
+        this.context = context;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent){
+        if(view == null){
+            view = inflater.inflate(R.layout.layout_voiture, parent, false);
+        }
+        TextView tvMarque =  view.findViewById(R.id.tvMarque);
+        TextView tvModel =  view.findViewById(R.id.tvModel);
+        TextView tvTel =  view.findViewById(R.id.tvNumeroTel);
+        TextView tvPrix = view.findViewById(R.id.tvPrix);
+        ImageView image = view.findViewById(R.id.imgView);
+        ImageButton imageButton = view.findViewById(R.id.btnTel);
+
+    }
 }
