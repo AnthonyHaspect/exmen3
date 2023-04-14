0fb634ffb149544aa22bdad955154def9c8618ca
Anthony Haspect <2027425@cegepoutaouais.qc.ca>
2023-04-14 09:07:21 -0400

    2 image faite

--- app/src/main/java/com/anthonyhaspect/anthonyhaspect/Voiture.java
+++ app/src/main/java/com/anthonyhaspect/anthonyhaspect/Voiture.java
@@ -1,2 +1,16 @@
-package com.anthonyhaspect.anthonyhaspect;public class Voiture {
+package com.anthonyhaspect.anthonyhaspect;
+
+
+public class Voiture {
+    String marque;
+    String model;
+    String numTel;
+    Integer prix;
+
+    public Voiture(String marque, String model, String numTel, Integer prix ){
+        this.marque = marque;
+        this.model = model;
+        this.numTel = numTel;
+        this.prix = prix;
+    }
 }
--- app/src/main/java/com/anthonyhaspect/anthonyhaspect/VoitureAdapteur.java
+++ app/src/main/java/com/anthonyhaspect/anthonyhaspect/VoitureAdapteur.java
@@ -1,2 +1,4 @@
-package com.anthonyhaspect.anthonyhaspect;public class VoitureAdapteur {
+package com.anthonyhaspect.anthonyhaspect;
+
+public class VoitureAdapteur {
 }
